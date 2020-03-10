2e909b8ae4de9420b4dc93b1f319187387e972c5
K Rhoda <kelseydrhoda@gmail.com>
2020-03-10 14:34:33 -0600

    Major polish to pichan. Added errors, transparent state, and removed all but essential locks

--- pi_chan/src/lib.rs
+++ pi_chan/src/lib.rs
@@ -1,17 +1,67 @@
-use std::sync::{Arc, Condvar, Mutex};
+use std::error::Error;
+use std::fmt;
+use std::sync::{Arc, Mutex};
 use wait_group::WaitGroup;
 
-pub type Guard = Arc<(Mutex<bool>, Condvar)>;
-
 #[derive(Debug)]
-pub struct UsedChanErr;
+pub enum ChanError {
+    UsedSendChanError,
+    UsedRecvChanError,
+    UninitializedChanError,
+}
+
+impl fmt::Display for ChanError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ChanError::UsedSendChanError => write!(f, "Cannot Send on PiChan more than once"),
+            ChanError::UsedRecvChanError => write!(f, "Cannot Recieve on PiChan more than once"),
+            ChanError::UninitializedChanError => {
+                write!(f, "PiChan must be initialized to use safely")
+            }
+        }
+    }
+}
+
+impl Error for ChanError {
+    fn description(&self) -> &str {
+        match self {
+            ChanError::UsedSendChanError => "Cannot Send on PiChan more than once",
+            ChanError::UsedRecvChanError => "Cannot Recieve on PiChan more than once",
+            ChanError::UninitializedChanError => "PiChan must be initialized to use safely",
+        }
+    }
+
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        None
+    }
+}
 
 #[derive(Clone)]
 pub struct PiChan<T>(Arc<PiMachine<T>>);
 
+#[derive(Debug)]
+pub enum PiChanState {
+    Unintialized, // Shouldn't happen, but who knows what someone will do.
+    Open,         // Neither Send or Recieve is Used.
+    AwaitSend,    // A listener is waiting.
+    AwaitRecv,    // A sender is waiting.
+    Used,         // A transfer was made.
+}
+
+impl fmt::Display for PiChanState {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            PiChanState::Unintialized => write!(f, "Uninitalized (ILLEGAL, USE PiChan::<T>::new)"),
+            PiChanState::Open => write!(f, "Open"),
+            PiChanState::AwaitSend => write!(f, "AwaitngSend"),
+            PiChanState::AwaitRecv => write!(f, "AwaitingRecv"),
+            PiChanState::Used => write!(f, "Used"),
+        }
+    }
+}
+
 struct PiMachine<T> {
-    init: bool,
-    used: bool,
+    init: Arc<Mutex<bool>>,
     val: Arc<Mutex<Option<T>>>,
     send_guard: Arc<Mutex<bool>>,
     send_wg: WaitGroup,
@@ -21,16 +71,17 @@ struct PiMachine<T> {
 
 impl<T> PiChan<T> {
     pub fn new() -> PiChan<T> {
+        // TODO: Consider changing to a barrier?
         let swg = WaitGroup::new();
         let rwg = WaitGroup::new();
 
-        // Preparting for each thread to decrement the other's group, creating a barrier.
+        // Preparing for each thread to decrement the other's group, creating a barrier.
+        // This is the importance of the init field.
         swg.add(1);
         rwg.add(1);
 
         PiChan::<T>(Arc::new(PiMachine::<T> {
-            init: true,
-            used: false,
+            init: Arc::new(Mutex::new(true)),
             val: Arc::new(Mutex::new(None)),
             send_guard: Arc::new(Mutex::new(false)),
             send_wg: swg,
@@ -39,62 +90,82 @@ impl<T> PiChan<T> {
         }))
     }
 
-    fn is_in_flight(&self) -> bool {
-        self.0.init && !self.0.used
-    }
-
-    pub fn is_used(&self) -> bool {
-        !self.is_in_flight()
+    pub fn state(&self) -> PiChanState {
+        match self.check_init() {
+            false => PiChanState::Unintialized,
+            _ => match self.check_send_used() {
+                true => match self.check_recv_used() {
+                    true => PiChanState::Used,
+                    _ => PiChanState::AwaitRecv
+                }
+                _ => match self.check_recv_used() {
+                    true => PiChanState::AwaitSend,
+                    _ => PiChanState::Open
+                }
+            }
+        }
     }
 
     // TODO: Optimize. Destroying channel == less locks.
-    pub fn send(&mut self, t: T) -> Result<(), UsedChanErr> {
-        // Prevent other senders, hold the lock.
-        let r = self.check_send_used();
-        match r {
-            Err(x) => Err(x),
+    pub fn send(&mut self, t: T) -> Result<(), ChanError> {
+        match self.check_init() {
+            // We have come into the possesion of an uninitialized channel through spectacular means.
+            false => Err(ChanError::UninitializedChanError),
+            true => {
+                let r = self.set_send_used();
 
-            Ok(()) => {
-                // Detect Recieve.
-                self.0.recv_wg.wait();
+                match r {
+                    // We are not the winning sender, the channel has been used.
+                    Err(x) => Err(x),
 
-                // finally.
-                let mut data = self.0.val.lock().unwrap();
-                *data = Some(t);
+                    Ok(()) => {
+                        // Detect Recieve.
+                        self.0.recv_wg.wait();
 
-                // Inform recieve we exist
-                self.0.send_wg.done();
+                        // finally.
+                        let mut data = self.0.val.lock().unwrap();
+                        *data = Some(t);
 
-                // Weaken references to self?
-                // If so, one here.
-                Ok(())
+                        // Inform recieve we exist
+                        self.0.send_wg.done();
+
+                        // Weaken references to self?
+                        // If so, one here.
+                        Ok(())
+                    }
+                }
             }
         }
     }
 
-    pub fn recv(&mut self) -> Result<Option<T>, UsedChanErr> {
-        let r = self.check_recv_used();
-        match r {
-            Err(x) => Err(x),
-            Ok(()) => {
-                self.0.recv_wg.done(); // Alert the sender.
+    pub fn recv(&mut self) -> Result<Option<T>, ChanError> {
+        match self.check_init() {
+            // We have come into the possesion of an uninitialized channel through spectacular means.
+            false => Err(ChanError::UninitializedChanError),
+            true => {
+                let r = self.set_recv_used();
+                match r {
+                    Err(x) => Err(x),
+                    Ok(()) => {
+                        self.0.recv_wg.done(); // Alert the sender.
 
-                self.0.send_wg.wait(); // Await the sender.
+                        self.0.send_wg.wait(); // Await the sender.
 
-                // Weaken references to self?
-                Ok(self.0.val.lock().unwrap().take())
-                // If so, one here after assigning take.
-                // Then return the assigned take.
+                        // Weaken references to self?
+                        Ok(self.0.val.lock().unwrap().take())
+                        // If so, one here after assigning take.
+                        // Then return the assigned take.
+                    }
+                }
             }
         }
-
     }
 
-    fn check_send_used(&mut self) -> Result<(), UsedChanErr> {
+    fn set_send_used(&mut self) -> Result<(), ChanError> {
         let mut is_used = self.0.send_guard.lock().unwrap();
 
         match *is_used {
-            true => Err(UsedChanErr),
+            true => Err(ChanError::UsedSendChanError),
             false => {
                 *is_used = true;
                 Ok(())
@@ -102,15 +173,27 @@ impl<T> PiChan<T> {
         }
     }
 
-    fn check_recv_used(&mut self) -> Result<(), UsedChanErr> {
+    fn set_recv_used(&mut self) -> Result<(), ChanError> {
         let mut is_used = self.0.recv_guard.lock().unwrap();
 
         match *is_used {
-            true => Err(UsedChanErr),
+            true => Err(ChanError::UsedRecvChanError),
             false => {
                 *is_used = true;
                 Ok(())
             }
         }
+    }
+
+    fn check_send_used(&self) -> bool {
+        *self.0.send_guard.lock().unwrap()
+    }
+
+    fn check_recv_used(&self) -> bool {
+        *self.0.recv_guard.lock().unwrap()
+    }
+
+    fn check_init(&self) -> bool {
+        *self.0.init.lock().unwrap()
     }
 }
--- runner/src/main.rs
+++ runner/src/main.rs
@@ -3,13 +3,14 @@ use pi_chan;
 // IN PROGRESS.
 // use spark;
 
-use wait_group;
 use std::thread;
+use wait_group;
 
 fn main() {
     println!("Hello, from Thread 1!");
     run_pi_chan();
-    run_wait_group()
+    run_wait_group();
+    pi_chan_state();
 }
 
 fn run_wait_group() {
@@ -64,7 +65,72 @@ fn run_pi_chan() {
             println!("Thread 2: Heard Err Listening to c1");
         }
     }
+}
 
+fn pi_chan_state() {
+    let mut p1 = pi_chan::PiChan::<usize>::new();
+    let mut q1 = p1.clone();
+
+    let mut p2 = pi_chan::PiChan::<usize>::new();
+    let mut q2 = p2.clone();
+
+    let un_init = p1.state();
+    match un_init {
+        pi_chan::PiChanState::Open => println!("P1 is open as expected"),
+        _ => println!("P1 was in unexpected state! {}", un_init),
+    }
+
+    let un_init = p2.state();
+    match un_init {
+        pi_chan::PiChanState::Open => println!("P2 is open as expected"),
+        _ => println!("P2 was in unexpected state! {}", un_init),
+    }
+
+    let h = thread::spawn(move || {
+        let non_determ = q1.state();
+        match non_determ {
+            pi_chan::PiChanState::Open => println!("Q1 is open, we are ahead of the main thread"),
+            pi_chan::PiChanState::AwaitRecv => {
+                println!("Q1 is awaiting a reciever, we are behind the main thread")
+            }
+            _ => println!("Q1 is in an unexpected state!, {}", non_determ),
+        }
+        q1.recv().expect("ERR IN Q1");
+
+        let used = q1.state();
+        match used {
+            pi_chan::PiChanState::Used => println!("Q1 is used as expected"),
+            _ => println!("Q1 was in unexpected state! {}", used),
+        }
+
+        let non_determ = q2.state();
+        match non_determ {
+            pi_chan::PiChanState::Open => println!("Q2 is open, we are ahead of the main thread"),
+            pi_chan::PiChanState::AwaitSend => println!("Q2 is awaiting a sender, we are behind the main thread"),
+            _ => println!("Q1 is in an unexpected state!, {}", non_determ),
+        }
+
+        q2.send(1).expect("ERR IN Q2");
+
+        let used = q2.state();
+        match used {
+            pi_chan::PiChanState::Used => println!("Q2 is used as expected"),
+            _ => println!("Q2 was in unexpected state! {}", used),
+        }
+
+        println!("Goodbye from thread 2");
+    });
+
+    p1.send(1).expect("ERR IN P1");
+    p2.recv().expect("ERR IN P2");
+
+    let used = p1.state();
+    match used {
+        pi_chan::PiChanState::Used => println!("P1 is used as expected"),
+        _ => println!("P1 was in unexpected state! {}", un_init),
+    }
+    h.join();
+    println!("Goodbye from thread 1");
 }
 
 // fn test_spark() {
