b0b166ac17544cce25ae6d11ba9b139a2e2589a9
K Rhoda <kelseydrhoda@gmail.com>
2020-03-12 18:38:45 -0600

    Abstracted out PropResult as a safer, faster way to respresent PropChan

--- prop_chan/src/lib.rs
+++ prop_chan/src/lib.rs
@@ -1,6 +1,6 @@
 use std::error::Error;
 use std::fmt;
-use std::sync::{Arc, Mutex, RwLock};
+use std::sync::{Arc, Mutex, RwLock, LockResult, RwLockReadGuard, RwLockWriteGuard};
 use wait_group::WaitGroup;
 
 // The Theory:
@@ -8,7 +8,9 @@ use wait_group::WaitGroup;
 // Similar to PiChan, permits only one sender.
 // Dissimilar to PiChan, this sender does not block and there can be many recievers.
 // The recievers are halted or sent away (if sampling) if the send has not yet occured.
-// Once the send has occured, an Arc<Mutex<Option<T>>> is cloned per-reciever.
+// The result is a guarded form of the RwLock provided by the standard lib, but not permitting anyone to write.
+// Provides a read function which returns the lock guard from the underlying lock, making the borrow-checker happy.
+// The lock will never be contested because the sole writer must conclude before the first reader reads.
 
 // AS WITH PiChan CONSIDER FOR DEADLOCK FREEDOM:
 // Breaking into sender + reciever, killing a wait if the other drops from exisitence.
@@ -22,6 +24,33 @@ use wait_group::WaitGroup;
 // It is best to think of this as a future that was run once then cached.
 #[derive(Clone)]
 pub struct PropChan<T>(Arc<PropMachine<T>>);
+
+// A thread-safe read-only instance of a variable deposited by send.
+// Exposes read, which returns the RwLockReadGuard of the underlying RwLock
+// Other arcitecture prevents a reader from reading before the sole write,
+// Or the writer holding on to the lock for longer than the time to write,
+// so the read function should never be contested, contain a poisoned err, or block.
+// AND the borrow checker is happy.
+#[derive(Clone)]
+pub struct PropResult<T>(Arc<RwLock<Option<T>>>);
+
+impl<T> PropResult<T> {
+    pub fn read(&self) -> LockResult<RwLockReadGuard<'_, Option<T>>> {
+        self.0.read()
+    }
+
+    pub fn clone(&self) -> PropResult<T> {
+        PropResult::<T>(Arc::clone(&self.0))
+    }
+
+    fn write(&mut self) -> LockResult<RwLockWriteGuard<'_, Option<T>>> {
+        self.0.write()
+    }
+
+    fn new(a: Arc<RwLock<Option<T>>>) -> PropResult<T> {
+        PropResult::<T>(a)
+    }
+}
 
 #[derive(Debug)]
 pub enum PropChanState {
@@ -44,7 +73,7 @@ impl fmt::Display for PropChanState {
 
 struct PropMachine<T> {
     init: Arc<Mutex<bool>>,
-    val: Arc<RwLock<Option<T>>>,
+    val: Arc<Mutex<PropResult<T>>>, 
     send_guard: Arc<Mutex<bool>>,
     recv_wg: WaitGroup,
 }
@@ -62,12 +91,13 @@ impl<T> PropChan<T> {
 
         PropChan::<T>(Arc::new(PropMachine::<T> {
             init: Arc::new(Mutex::new(true)),
-            val: Arc::new(RwLock::new(None)),
+            val: Arc::new(Mutex::new(PropResult::new(Arc::new(RwLock::new(None))))),
             send_guard: Arc::new(Mutex::new(false)),
             recv_wg: recv_wg,
         }))
     }
 
+    // Check the state of a given PropChan
     pub fn state(&self) -> PropChanState {
         match self.check_init() {
             false => PropChanState::Unintialized,
@@ -78,6 +108,8 @@ impl<T> PropChan<T> {
         }
     }
 
+    // Attempt to deposit a value into the channel.
+    // If not the first, or if the channel was not built using new(), returns an error.
     pub fn send(&mut self, t: T) -> Result<(), PropChanError> {
         match self.check_init() {
             // We have come into the possession of an uninitialized channel through spectacular means.
@@ -90,7 +122,8 @@ impl<T> PropChan<T> {
                     Err(x) => Err(x),
 
                     Ok(()) => {
-                        let mut data = self.0.val.write().unwrap();
+                        let mut wrapper = self.0.val.lock().unwrap();
+                        let mut data = wrapper.write().unwrap();
                         *data = Some(t);
 
                         self.0.recv_wg.done();
@@ -104,13 +137,15 @@ impl<T> PropChan<T> {
         }
     }
 
-    pub fn recv(&mut self) -> Result<Arc<RwLock<Option<T>>>, PropChanError> {
+    // recv on an initialized channel returns a PropResult which can freely be read from across threads.
+    // Blocks until PropResult is ready.
+    pub fn recv(&mut self) -> Result<PropResult<T>, PropChanError> {
         match self.check_init() {
             // We have come into the possesion of an uninitialized channel through spectacular means.
             false => Err(PropChanError::UninitializedChanError),
             true => {
                 self.0.recv_wg.wait();
-                Ok(Arc::clone(&self.0.val))
+                Ok(self.0.val.lock().unwrap().clone())
             }
         }
     }
@@ -119,7 +154,7 @@ impl<T> PropChan<T> {
     // Like recieve, but non-blocking. Instead immediately returns a tuple
     // The first element is a bool indicating if send has occured, and the second element is
     // Either a clone of the Arc<Mutex<Option<TargetValue>>>, or a wrapper around a none.
-    pub fn sample(&mut self) -> Result<(bool, Arc<RwLock<Option<T>>>), PropChanError> {
+    pub fn sample(&mut self) -> Result<(bool, PropResult<T>), PropChanError> {
         match self.check_init() {
             // We have come into the possession of an uninitialized channel through spectacular means.
             false => Err(PropChanError::UninitializedChanError),
@@ -128,13 +163,13 @@ impl<T> PropChan<T> {
                 let is_complete = self.0.send_guard.lock().unwrap();
 
                 match *is_complete {
-                    false => Ok((false, Arc::new(RwLock::new(None)))),
+                    false => Ok((false, PropResult::<T>::new(Arc::new(RwLock::new(None))))),
                     _ => {
                         // We might be right alongside the sender.
                         // In practice, should not block.
                         self.0.recv_wg.wait();
 
-                        Ok((true, Arc::clone(&self.0.val)))
+                        Ok((true, self.0.val.lock().unwrap().clone()))
                     }
                 }
             }
